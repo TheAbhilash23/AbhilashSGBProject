86ba5939c06c007df282607d47ac8f501924825e
TheAbhilash23 <theak.92@gmail.com>
2022-07-19 14:07:19 +0530

    after dumping

--- SGBproject/models.py
+++ SGBproject/models.py
@@ -29,12 +29,3 @@ class Odb(models.Model):
     #     abstract = True       # this tells Django, not to create a database table for the corresponding table.
 
 
-class CustEnquiry(models.Model):
-    custid = models.AutoField(primary_key=True,
-                              serialize=False,
-                              verbose_name='ID')
-    name = models.CharField(max_length=25)
-    telephone = models.CharField(max_length=10)
-    email = models.EmailField(max_length=50)
-    socialmedia = models.CharField(max_length=155)
-    formal_education = models.CharField(max_length=50)
--- SGBproject/settings.py
+++ SGBproject/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'SGBproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'den88b2j2lt17l',
+        'USER': 'pfmrhofafrezns',
+        'PASSWORD': '695fbf8539b3828781619e669407cfd81789086602001dbb5cf255c154cc35e5',
+        'HOST': 'ec2-3-219-52-220.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
